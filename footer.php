d23e247500e524b4b39f4a073387369250133c01
DrKat123 <raphaxander@gmail.com>
2016-08-17 17:39:52 +0700

    update

---
+++ footer.php
@@ -0,0 +1,24 @@
+<!--Footer is from http://tutorialzine.com/2015/01/freebie-5-responsive-footer-templates/-->
+<footer class="footer-distributed">
+	<div class="footer-right">
+		<a href="https://twitter.com/sharpscratchmod"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+		<a href="https://sharpscratchmod.cf"><i class="fa fa-globe" aria-hidden="true"></i></a>
+		<a href="https://github.com/sharpscratchmod"><i class="fa fa-github" aria-hidden="true"></i></a>
+		<a href=""><i class="fa fa-google-plus-official" aria-hidden="true"></i></a>
+
+	</div>
+	<div class="footer-left">
+		<p class="footer-links">
+			<a href="index.php">Home</a>
+					·
+			<a href="blog.sharpscratchmod.cf">Blog</a>
+					·
+			<a href="#">About</a>
+					·
+			<a href="#">Contact</a>
+		</p>
+
+		<p>&copy; 2016 Sharp Scratch Mod Developers</p>
+	</div>
+
+</footer>
---
+++ header.php
@@ -0,0 +1,12 @@
+<nav class="navbar">
+	<ul>
+		<li class="navbar-title"><a href="#">Sharp</a></li>
+		<li><a href="https://sharpscratchmod.cf/Sharp.swf">Create</a></li>
+		<li><a href="#">Explore</a></li>
+		<li><a href="https://forums.sharpscratchmod.cf/">Discuss</a></li>
+		<li><a href="#">About</a></li>
+		<li><a href="#">Help</a></li>
+		<li><a href="#"><?php print "Login"; ?></a></li>
+		<li><a href="register.php"><?php print "Register"; ?></a></li>
+	</ul>
+</nav>
