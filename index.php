2ba754794107d4f8f396df82e6bfd977f35a4fa5
DrKat123 <raphaxander@gmail.com>
2016-08-17 17:35:10 +0700

    Updates

--- index.php
+++ index.php
@@ -3,21 +3,16 @@
 	<head>
 		<link rel="stylesheet" href="../sharp-css/build/sharp-css.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 		<script src="../sharp-css/sharp-css.js"></script> <!--IRONIC-->
 	</head>
 	<body>
-		<nav class="navbar">
-			<ul>
-				<li class="navbar-title"><a href="#">Sharp</a></li>
-				<li><a href="https://sharpscratchmod.cf/Sharp.swf">Create</a></li>
-				<li><a href="#">Explore</a></li>
-				<li><a href="https://forums.sharpscratchmod.cf/">Discuss</a></li>
-				<li><a href="#">About</a></li>
-				<li><a href="#">Help</a></li>
-				<li><a href="#"><?php print "Login"; ?></a></li>
-				<li><a href="#"><?php print "Register"; ?></a></li>
-			</ul>
-		</nav>
+		<?php include("header.php");?>
 		<p>Sharp is a <a href="https://scratch.mit.edu/?href=sharpscratchmod.cf">Scratch 2.0</a> mod</p>
+		<br>
+		<br>
+		<br>
+		<br>
+		<?php include("footer.php"); ?>
 	</body>
 </html>
